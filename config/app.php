8fb5d476b1c224b042a95a6f29849d4f88c0caa0
Jerez Bain <jerezb31@gmail.com>
2018-05-05 14:37:07 -0400

    try registration mail 2
    Signed-off-by: Jerez Bain <jerezb31@gmail.com>

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Bogardo\Mailgun\MailgunServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -208,6 +208,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Mailgun' => Bogardo\Mailgun\Facades\Mailgun::class
 
     ],
 
